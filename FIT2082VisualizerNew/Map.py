135b3ba8d6948bcf9a209daae51edc28f85beaea
xlii0151 <xlii0151@student.monash.edu>
2021-09-11 01:04:28 +1000

    add backward and forward function

--- FIT2082VisualizerNew/Map.py
+++ FIT2082VisualizerNew/Map.py
@@ -44,12 +44,7 @@ class info:
             zip_iterator = zip(cons_time, loc_zip)
             self.constraint_dict = dict(zip_iterator)
             # print(self.constraint_dict)
-            # for i in range(len(self.agents)):
-            #     if len(self.agents[i])>109:
-            #         print(i)
-            #         print(self.agents[i][107])
-            #         print("!")
-            # print(self.agents)
+
         def draw_map(self,canvas):
             self.GridMap=self.Arrmap
             for x in range(len(self.Arrmap[0])):  #165
@@ -76,45 +71,63 @@ class info:
 
 
                 frame.text.insert("end", "Agent "+str(i) + ": "+"("+str(self.agents[i][0][0]-1)+","+str(self.agents[i][0][1]-1)+")"+"\n")
-        def move_agents(self,t,canvas,frame):
-            tt=1
-            frame.text.insert("end","\nTimestep: "+str(t)+"\n",'current')
+        def move_agents(self,t,canvas,frame,doBackward):
+            if doBackward:
+                for i in range(len(self.agents)):
+                    #if still moving.
+                    if 0<t<len(self.agents[i]):
+                        canvas.move(self.image[i],(self.agents[i][t-1][1]-self.agents[i][t][1])*self.a,(self.agents[i][t-1][0]-self.agents[i][t][0])* self.a)
+                    elif t==len(self.agents[i]):
+                        canvas.itemconfig(self.image[i], fill='#B45B3E')
+                        #reduce the oval's size once it reaches the destination
+                        #because cosntraint might later generate on it
+                        x0, y0, x1, y1 = canvas.coords(self.image[i])
+                        x0 = x0 - 2
+                        x1 = x1 + 2
+                        y0 = y0 - 2
+                        y1 = y1 + 2
+                        canvas.coords(self.image[i], x0, y0, x1, y1)
+                    #t=0,do nothing
+            else:
+                tt=1
+                frame.text.insert("end","\n")
+                frame.text.insert("end","Timestep: "+str(t)+"\n",'current')
 
-            if (t in self.constraint_dict.keys()):
-                canvas.itemconfig(self.GridMap[self.constraint_dict[t][0][0]][self.constraint_dict[t][0][1]], fill='#0000FF')
-                #add text description to the frame
-                frame.text.insert("end", "Constraint time "+str(t) + ":\n"+"("+str(self.constraint_dict[t][0][0]-1)+","+str(self.constraint_dict[t][0][1]-1)+")"+"\n",'constraint')
-                frame.text.see("end")
-                if (self.constraint_dict[t][1][0],self.constraint_dict[t][1][1]) != (0,-1):
-                    canvas.itemconfig(self.GridMap[self.constraint_dict[t][1][0]][self.constraint_dict[t][1][1]], fill='#0000FF')
-
-            for i in range(len(self.agents)):
-                #if still moving.
-                if t<len(self.agents[i]):
-                    canvas.move(self.image[i],(self.agents[i][t][1]-self.agents[i][t-1][1])*self.a,(self.agents[i][t][0]-self.agents[i][t-1][0])* self.a)
+                if (t in self.constraint_dict.keys()):
+                    canvas.itemconfig(self.GridMap[self.constraint_dict[t][0][0]][self.constraint_dict[t][0][1]], fill='#0000FF')
                     #add text description to the frame
-                    frame.text.insert("end", "Agent "+str(i) + ": "+"("+str(self.agents[i][t][0]-1)+","+str(self.agents[i][t][1]-1)+")"+"\n")
+                    frame.text.insert("end", "Constraint time "+str(t) + ":\n"+"("+str(self.constraint_dict[t][0][0]-1)+","+str(self.constraint_dict[t][0][1]-1)+")"+"\n",'constraint')
                     frame.text.see("end")
-                    #an agent is still moving, so repeater continues
-                    tt=0
-                #elif the agent stops, change its color to green
-                elif t==len(self.agents[i]):
-                    canvas.itemconfig(self.image[i], fill='#00FF00')
-                    #reduce the oval's size once it reaches the destination
-                    #because cosntraint might later generate on it
-                    x0, y0, x1, y1 = canvas.coords(self.image[i])
-                    x0 = x0 + 2
-                    x1 = x1 - 2
-                    y0 = y0 + 2
-                    y1 = y1 - 2
-                    canvas.coords(self.image[i], x0, y0, x1, y1)
-                #elif the agent have stopped, move (0,0) so speed don't accelerate
-                elif t>len(self.agents[i]) and t<=self.max_agents_length:
-                    canvas.move(self.image[i],0,0)
-                else:
-                    print("error?")
-            #return True if no agent is moving
-            return tt
+                    if (self.constraint_dict[t][1][0],self.constraint_dict[t][1][1]) != (0,-1):
+                        canvas.itemconfig(self.GridMap[self.constraint_dict[t][1][0]][self.constraint_dict[t][1][1]], fill='#0000FF')
+
+                for i in range(len(self.agents)):
+                    #if still moving.
+                    if t<len(self.agents[i]):
+                        canvas.move(self.image[i],(self.agents[i][t][1]-self.agents[i][t-1][1])*self.a,(self.agents[i][t][0]-self.agents[i][t-1][0])* self.a)
+                        #add text description to the frame
+                        frame.text.insert("end", "Agent "+str(i) + ": "+"("+str(self.agents[i][t][0]-1)+","+str(self.agents[i][t][1]-1)+")"+"\n")
+                        frame.text.see("end")
+                        #an agent is still moving, so repeater continues
+                        tt=0
+                    #elif the agent stops, change its color to green
+                    elif t==len(self.agents[i]):
+                        canvas.itemconfig(self.image[i], fill='#00FF00')
+                        #reduce the oval's size once it reaches the destination
+                        #because cosntraint might later generate on it
+                        x0, y0, x1, y1 = canvas.coords(self.image[i])
+                        x0 = x0 + 2
+                        x1 = x1 - 2
+                        y0 = y0 + 2
+                        y1 = y1 - 2
+                        canvas.coords(self.image[i], x0, y0, x1, y1)
+                    #elif the agent have stopped, move (0,0) so speed don't accelerate
+                    elif t>len(self.agents[i]) and t<=self.max_agents_length:
+                        canvas.move(self.image[i],0,0)
+                    else:
+                        print("error?")
+                #return True if no agent is moving
+                return tt
 
 if __name__=="__main__":
     temp=info("test_25.txt","warehouse-10-20-10-2-1.map.ecbs")
--- FIT2082VisualizerNew/run.py
+++ FIT2082VisualizerNew/run.py
@@ -3,29 +3,42 @@ import sys
 from tkinter import *
 from Map import info
 import time
-from tkinter import ttk
+
 def simulation_canvas  (parent,**config):
     global the_canvas
     the_canvas = Canvas(parent,**config)
     return the_canvas
 
 def repeater(root):
-    global Info,the_canvas,the_frame
+    global Info,the_canvas,the_frame,t,backward,forward
     t=1
-    while 1:
-        if continuePlay:
-            tt=Info.move_agents(t,the_canvas,the_frame)
+    while True:
+        if backward and t>1:
+            t-=1
+            Info.move_agents(t,the_canvas,the_frame,True)
+
+        elif forward and t>=1:
+            tt=Info.move_agents(t,the_canvas,the_frame,False)
             if tt:
                 break
             t+=1
+
+        elif continuePlay:
+            tt=Info.move_agents(t,the_canvas,the_frame,False)
+            if tt:
+                break
+            t+=1
+
+        forward=False
+        backward=False
+
         root.update()
         time.sleep(0.4)
 
 
-class Example(Frame):
+class myFrame(Frame):
     def __init__(self, *args, **kwargs):
         Frame.__init__(self, *args, **kwargs)
-
         self.text = Text(self, height=6, width=20)
         self.vsb = Scrollbar(self, orient="vertical", command=self.text.yview)
         self.text.configure(yscrollcommand=self.vsb.set)
@@ -33,27 +46,50 @@ class Example(Frame):
         self.text.pack(side="left", fill="both", expand=True)
         self.text.tag_config('constraint', background="#DCE2F1", foreground="black")
         self.text.tag_config('current', background="#EBEBE4", foreground="black")
-        self.button = Button(self, text='Play',width=5,height=2, bg='green', fg='black', command=self.play_agents)
+        self.button = Button(self, text='Play',width=5,height=2, bg='green', fg='black', command=self.play_visualizer)
         self.button.pack(side=TOP, padx=5, pady=5)
 
+        self.button1 = Button(self, text='<<',width=5,height=2, bg='#EBEBE4', fg='black', command=self.backward)
+        self.button1.pack(side=BOTTOM, padx=5, pady=5)
 
-    def play_agents(self):
-        global continuePlay
+        self.button2 = Button(self, text='>>',width=5,height=2, bg='#EBEBE4', fg='black', command=self.forward)
+        self.button2.pack(side=BOTTOM, padx=5, pady=5)
+
+
+    def play_visualizer(self):
+        global continuePlay,Paused
         if self.button["text"] == "Play":
             self.button["text"] = "Pause"
             self.button["bg"] = "red"
             continuePlay=False
+
         else:
             self.button["text"] = "Play"
             self.button["bg"] = "green"
             continuePlay= True
+
+    def backward(self):
+        global continuePlay,t,backward,forward
+        self.button["text"] = "Pause"
+        self.button["bg"] = "red"
+        backward=True
+        forward=False
+        continuePlay=False
+
+    def forward(self):
+        global continuePlay,t,forward,backward
+        self.button["text"] = "Pause"
+        self.button["bg"] = "red"
+        forward=True
+        backward=False
+        continuePlay=False
 
 
 
 if __name__=="__main__":
     agents_file=sys.argv[1]
     map_file=sys.argv[2]
-    global Info,the_canvas,the_frame,continuePlay
+    global Info,the_canvas,the_frame,continuePlay,t,backward,forward
     # addagen,addmap="test_25.txt","warehouse-10-20-10-2-1.map.ecbs"
 
     addagen,addmap=agents_file,map_file
@@ -64,16 +100,15 @@ if __name__=="__main__":
     the_canvas   = None
     root = Tk()
 
-    continuePlay = True
+    continuePlay,backward,forward = True,False,False
 
     root.title("Simulation")
     # root.protocol("WM_DELETE_WINDOW",exit)
 
-    the_frame = Example(root)
+    the_frame = myFrame(root)
     the_frame.pack(side="right",fill="both", expand=True)
 
     simulation_canvas(root,width=1630,height=720,bg="#d1d1d1").pack(side=LEFT,expand=True,fill=BOTH)
-
 
 
     Info.draw_map(the_canvas)
