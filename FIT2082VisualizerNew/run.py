5845c506e167b0e53a1ef5009292cf96fe039071
xlii0151 <xlii0151@student.monash.edu>
2021-09-10 17:49:34 +1000

    command argv

--- FIT2082VisualizerNew/run.py
+++ FIT2082VisualizerNew/run.py
@@ -51,12 +51,12 @@ class Example(Frame):
 
 
 if __name__=="__main__":
-    # agents_file=sys.argv[1]
-    # map_file=sys.argv[2]
+    agents_file=sys.argv[1]
+    map_file=sys.argv[2]
     global Info,the_canvas,the_frame,continuePlay
-    addagen,addmap="test_25.txt","warehouse-10-20-10-2-1.map.ecbs"
+    # addagen,addmap="test_25.txt","warehouse-10-20-10-2-1.map.ecbs"
 
-    # addagen,addmap=agents_file,map_file
+    addagen,addmap=agents_file,map_file
 
     Info=info(addagen,addmap)
 
